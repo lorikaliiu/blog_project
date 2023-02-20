b3d31022e701ffc75b928da9d34b928ee62d7d8a
Lorik Aliu <aliulorik1@gmail.com>
2023-02-20 14:01:09 +0100

    fixing search

--- resources/views/post/indexPost.blade.php
+++ resources/views/post/indexPost.blade.php
@@ -10,11 +10,13 @@
             @csrf
             <ol class="breadcrumb">
                 <div class="input-group">
+                    <button class="btn bg-transparent">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                          </svg>
+                    </button>
                     <input list="q" name="q" type="text" class="form-control" placeholder="Search...">
                     <div class="input-group-addon">
-                        <button class="btn bg-transparent">
-                            <i class="fa fa-times"></i>
-                        </button>
                     </div>
                 </div>
             </ol>
@@ -29,7 +31,7 @@
                         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                             <thead>
                                 <tr>
-                                    <th>#</th>
+                                    <th>Id</th>
                                     <th>Category</th>
                                     <th>Title</th>
                                     <th>Image</th>
